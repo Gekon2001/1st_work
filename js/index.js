04aae3b78035aebe107c4fa4312e901cb4c6fbd9
Gekon2001 <gekon2001@ukr.net>
2018-05-30 14:44:15 +0300

    1st_commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('label[for="smart"]').click(function() {
+	$(".showText").html("Your cloud phone system follows you<br/> wherever you go, so you never miss<br/> a call.")
+});
+$('label[for="simple"]').click(function() {
+	$(".showText").html("The technology is seamlessly wrapped up in a remarkably intuitive interface that works across desk phones, smartphones, tablets, and computers.")
+});	
---
+++ readme.txt
@@ -0,0 +1,9 @@
+1. Responsive markup (single breakepoint - 768px)
+2. LESS
+3. SVG icons (export from PSD)
+4. Lato Google Font
+5. JQuery
+6. Performance optimization
+
+!!IMPORTANT!!
+Pure code, no bundlers, prefixers etc.
